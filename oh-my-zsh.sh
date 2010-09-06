94dd050723a19534f39a9a65b854f5e8f129b270
Richard Grundy <rich@27smiles.com>
2010-09-06 10:00:11 +0100

    Added RVM completion stuff, if it exists.

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -3,6 +3,12 @@
 # add a function path
 fpath=($ZSH/functions $fpath)
 
+# add rvm completion if it exists.
+if [ -d "$HOME/.rvm/scripts/zsh/Completion" ]
+then
+  fpath=($HOME/.rvm/scripts/zsh/Completion $fpath)
+fi
+
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
 for config_file ($ZSH/lib/*.zsh) source $config_file
